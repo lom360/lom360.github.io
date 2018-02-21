e3c8b0f9aee83ee35f2f610358212dc6ad988002
lom360 <lom360@mail.fresnostate.edu>
2018-02-20 20:40:29 -0800

    Item number is able to increment.

--- projects/toDoApp/js/script.js
+++ projects/toDoApp/js/script.js
@@ -1,3 +1,4 @@
+var count = 0;
 var newItemForm = document.forms.newItem;
 newItemForm.addEventListener("submit", function(e){
     login(e)});
@@ -6,15 +7,25 @@ function User(a) {
     this.item = a;
 }
 
+function CheckCount() {
+    this.total = 0;
+}
 
 function login(e) {
     e.preventDefault();
+    count++;
+    if(count <= 7) {
+        var item = document.querySelector('.item').value;
+        var newUser = new User(item);
 
-    var item = document.querySelector('.item').value;
-    var newUser = new User(item);
 
-    var newList = document.createElement('li');
-    var textNode = document.createTextNode(newUser.item);
-    newList.appendChild(textNode);
-    document.getElementById("myList").appendChild(newList);
+        var newList = document.createElement('li');
+        var textNode = document.createTextNode(count + '. ' + newUser.item);
+        newList.appendChild(textNode);
+        document.getElementById("myList").appendChild(newList);
+    } else {
+        alert("Calm Down!!! You are planning too many things.")
+    }
+
+
 }
