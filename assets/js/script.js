a8d30e9f20bad7ee8b0aa22c0cf4e13dd9b7ec0b
lom360 <lom360@mail.fresnostate.edu>
2018-07-14 02:33:38 -0700

    Was able to console log scroll position

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,24 @@
-var navBar = document.querySelector("nav");
+var navBar = document.querySelector(".test");
+var last_known_scroll_position = 0;
+var ticking = false;
 
-console.log(navBar.scrollTop);
+// function navBackground(scroll_pos) {
+    
+// }
+
+window.addEventListener('scroll', function (e) {
+
+    last_known_scroll_position = window.scrollY;
+
+    if (!ticking) {
+
+        window.requestAnimationFrame(function () {
+            console.log(last_known_scroll_position);
+            ticking = false;
+        });
+
+        ticking = true;
+
+    }
+
+});
