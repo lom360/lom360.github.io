6d1873f694cad39d347858489232c680ad8d94a6
lom360 <lom360@mail.fresnostate.edu>
2018-07-15 04:38:19 -0700

    Refactored script.js file to be cleaner

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,18 @@
-var navBar = document.querySelector(".test");
-var last_known_scroll_position = 0;
-var tickTock = false;
+var navBar = document.querySelector("nav");
+var scrollPosition = 0;
+
+window.addEventListener('scroll', function (e) {
+    scrollPosition = window.scrollY;
+    checkTickTock(scrollPosition);
+});
+
+function checkTickTock(scrollPosition){
+    if(scrollPosition >= 200) {
+        addNavColor(scrollPosition);
+    } else {
+        removeNavColor(scrollPosition);
+    }
+}
 
 function addNavColor() {
     navBar.classList.add("nav-color");
@@ -8,27 +20,4 @@ function addNavColor() {
 
 function removeNavColor() {
     navBar.classList.remove("nav-color");
-}
-
-window.addEventListener('scroll', function (e) {
-
-    last_known_scroll_position = window.scrollY;
-    checkTickTock(last_known_scroll_position);
-    if (tickTock) {
-        window.requestAnimationFrame(function () {
-            addNavColor(last_known_scroll_position);
-        });
-    }else {
-        window.requestAnimationFrame(function () {
-            removeNavColor(last_known_scroll_position);
-        });
-    }
-});
-
-function checkTickTock(scroll_pos){
-    if(scroll_pos >= 200) {
-        tickTock = true;
-    } else {
-        tickTock = false;
-    }
 }
