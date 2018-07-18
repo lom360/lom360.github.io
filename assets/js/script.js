7a9ef729986aa520b4f28a4d8ea0c8157cec75b7
lom360 <lom360@mail.fresnostate.edu>
2018-07-17 23:45:16 -0700

    Working on fade in and fade out of navigation background color

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,27 @@
 var navBar = document.querySelector("nav");
 var scrollPosition = 0;
 
-window.addEventListener('scroll', function (e) {
-    scrollPosition = window.scrollY;
-    checkTickTock(scrollPosition);
+// window.addEventListener('scroll', function (e) {
+//     scrollPosition = window.scrollY;
+//     checkTickTock(scrollPosition);
+// });
+
+// function checkTickTock(scrollPosition){
+//     if(scrollPosition >= 30) {
+//         addNavColor(scrollPosition);
+//     } else {
+//         removeNavColor(scrollPosition);
+//     }
+// }
+
+// function addNavColor() {
+//     navBar.classList.add("sticky");
+// }
+
+// function removeNavColor() {
+//     navBar.classList.remove("sticky");
+// }
+
+$(window).scroll(function(){
+    $("nav").css("backgroundColor", "pink").fadeIn("slow");
 });
-
-function checkTickTock(scrollPosition){
-    if(scrollPosition >= 30) {
-        addNavColor(scrollPosition);
-    } else {
-        removeNavColor(scrollPosition);
-    }
-}
-
-function addNavColor() {
-    navBar.classList.add("sticky");
-}
-
-function removeNavColor() {
-    navBar.classList.remove("sticky");
-}
