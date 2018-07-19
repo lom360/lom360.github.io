4b6e1969783025618f02b9190a788108b2942266
lom360 <lom360@mail.fresnostate.edu>
2018-07-18 23:46:28 -0700

    Cleaned up code for navigation will make links work later

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,24 @@
-// var navBar = document.querySelector("nav");
-// var scrollPosition = 0;
-
-// window.addEventListener('scroll', function (e) {
-//     scrollPosition = window.scrollY;
-//     checkTickTock(scrollPosition);
-// });
-
-// function checkTickTock(scrollPosition){
-//     if(scrollPosition >= 75) {
-//         addNavColor(scrollPosition);
-//     } else {
-//         removeNavColor(scrollPosition);
-//     }
-// }
-
-// function addNavColor() {
-//     navBar.classList.add("sticky");
-// }
-
-// function removeNavColor() {
-//     navBar.classList.remove("sticky");
-// }
-
 var navOffset = $('nav').offset().top;
 
 $(window).scroll(function(){
     var scrollPos = $(window).scrollTop();
-
-    if(scrollPos >= navOffset) {
-        $('nav').addClass('sticky');
-        $('.nav-container').addClass('nav-background').fadeIn();
-    } else {
-        $('nav').removeClass('sticky');
-        $('.nav-container').removeClass('nav-background');        
-    }
+    checkOffset(scrollPos);
 });
 
+function checkOffset(scrollPos){
+    if (scrollPos >= navOffset) {
+        addNavFeatures();
+    } else {
+        removeNavFeatures();
+    }
+}
+
+function addNavFeatures() {
+    $('nav').addClass('sticky');
+    $('.nav-container').addClass('nav-background').fadeIn();
+}
+
+function removeNavFeatures() {
+    $('nav').removeClass('sticky');
+    $('.nav-container').removeClass('nav-background');
+}
