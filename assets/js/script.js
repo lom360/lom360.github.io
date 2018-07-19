df6834f784cd53606784f4345e35ae5d8aa3a09a
lom360 <lom360@mail.fresnostate.edu>
2018-07-19 00:18:43 -0700

    Fixed issue where nav would not appear after refresh

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
 var navOffset = $('nav').offset().top;
 
-$(window).scroll(function(){
+// After the annomynous function that there are extra paranthesis'
+// That's there so it can execute right away. We want to do that
+// so that way when we refresh the page no matter the scroll position
+// the nav background will apply appropriately.
+$(window).scroll(function () {
     var scrollPos = $(window).scrollTop();
     checkOffset(scrollPos);
-});
+}());
 
 function checkOffset(scrollPos){
     if (scrollPos >= navOffset) {
