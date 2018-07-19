c1f981966f6dddb8c5f55fb3e2e29eb98895cd16
lom360 <lom360@mail.fresnostate.edu>
2018-07-19 00:49:32 -0700

    Fixed more glitch on navigation and starting main content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,11 @@
-var navOffset = $('nav').offset().top;
+var navOffset = $('nav').offset().top; // assigns nav position
 
-// After the annomynous function that there are extra paranthesis'
-// That's there so it can execute right away. We want to do that
-// so that way when we refresh the page no matter the scroll position
-// the nav background will apply appropriately.
-$(window).scroll(function () {
-    var scrollPos = $(window).scrollTop();
-    checkOffset(scrollPos);
-}());
+// checkScroll() is called once so that way when page is refreshed
+// the navFeatures will apply appropriately.
+// So the following jQuery method is using the checkScroll argument
+// to apply the function appropriately constantly.
+checkScroll();
+$(window).scroll(checkScroll);
 
 function checkOffset(scrollPos){
     if (scrollPos >= navOffset) {
@@ -17,6 +15,11 @@ function checkOffset(scrollPos){
     }
 }
 
+// .sticky class applies position fixed to nav
+// .nav-background class applies background-color
+// They remain separate because when removing class
+// if they are combined will remove the contents of
+// the container altogether.
 function addNavFeatures() {
     $('nav').addClass('sticky');
     $('.nav-container').addClass('nav-background').fadeIn();
@@ -25,4 +28,9 @@ function addNavFeatures() {
 function removeNavFeatures() {
     $('nav').removeClass('sticky');
     $('.nav-container').removeClass('nav-background');
+}
+
+function checkScroll() {
+    var scrollPos = $(window).scrollTop();
+    checkOffset(scrollPos);
 }
